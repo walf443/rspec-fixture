1c84a9cba1255dc566e6a231cc8ef76f78b4592f
walf443 <walf443@d0d07461-0603-4401-acd4-de1884942a52>
2008-02-10 09:52:37 +0000

    lang/ruby/rspec-fixture: add spec for with_fixture method
    git-svn-id: http://svn.coderepos.org/share/lang/ruby/rspec-fixture/trunk@6502 d0d07461-0603-4401-acd4-de1884942a52

---
+++ spec/with_fixture_spec.rb
@@ -0,0 +1,33 @@
+require File.join(File.dirname(__FILE__), 'spec_helper')
+require 'spec/fixture'
+
+describe 'with_fixtures' do
+  it 'should defined in example group scope' do
+    lambda do
+      describe 'testing' do
+        with_fixtures do
+        end
+      end
+    end.should_not raise_error
+  end
+
+  it 'should work well in nested example groups' do
+    lambda do
+      describe 'foo' do
+        describe 'bar' do
+          with_fixtures do
+          end
+        end
+      end
+    end.should_not raise_error
+  end
+
+  it "should not defined in Kernel scope" do
+    lambda do
+      Kernel.instanse_eval do
+        with_fixtures do
+        end
+      end
+    end.should raise_error(NoMethodError)
+  end
+end
